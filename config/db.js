133877836c72e3f4468c05a793ae5a157afd9fcc
ellen24h <ellen24h@gmail.com>
2015-11-03 01:08:05 +0900

    modified min-length of input text

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
-
 var db = mongoose.connect('mongodb://localhost/ellenradiodb');
-autoIncrement.initialize(db);
+
 
